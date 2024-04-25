8741a0c77b2ce238115a5cbd155755379a24172d
pykido <animoto1@naver.com>
2024-04-25 18:11:57 +0900

    feat : 13913_숨바꼭질_4 태윤 풀이 (최종)

--- pykido/DFS | BFS/BOJ/13913_숨바꼭질_4.cpp
+++ pykido/DFS | BFS/BOJ/13913_숨바꼭질_4.cpp
@@ -2,7 +2,6 @@
 // 5 -> 4 -> 8 -> 16 -> 17
 // 5 -> 10 -> 9 -> 18 -> 17
 //
-// 1.
 
 
 #include <iostream>
@@ -13,33 +12,27 @@ using namespace std;
 #define MAX 100001
 
 int N, K; // N : 수빈이의 위치, K : 동생의 위치
-bool visited[MAX];
+int visited[MAX] = {}; // 전부 0으로 초기화
 int memo[MAX]; // 연산 과정 저장할 배열
-int result; // 동생 찾는 최단 시간
-vector<int> ans_vector; //
+vector<int> ans_vector; // 어떻게 이동하는지 저장하는 vector
 
 void bfs() {
-    queue<pair<int, int> > q;
-    q.push(make_pair(N, 0));
-
-    visited[N] = true;  // 시작 위치 방문 처리
+    queue<int> q;
+    q.push(N);
 
     while (!q.empty()) {
-        int curPosition = q.front().first;
-        int time = q.front().second;
+        int curPosition = q.front();
         q.pop();
-        visited[curPosition] = true;
 
         // 동생 찾았다면??
         if (curPosition == K) {
-            result = time;
-
             int i = curPosition; // 현재 위치 K
+
             while(i != N) {
                 ans_vector.push_back(i);
                 i = memo[i]; // i = 이전 위치
             }
-            ans_vector.push_back(i);
+            ans_vector.push_back(i); // i == N
 
             break;
         }
@@ -49,26 +42,29 @@ void bfs() {
         int multiPosition = curPosition * 2;
 
         // 1. -1
-        if (minusPosition >= 0 && !visited[minusPosition]) {
-            q.push(make_pair(minusPosition,time + 1));
+        if (minusPosition >= 0 && visited[minusPosition] == 0) {
+            q.push(minusPosition);
             memo[minusPosition] = curPosition;  // ⭐️ 나중 위치 인덱스가 현재 위치 값을 저장한다 !!
+            visited[minusPosition] = visited[curPosition] + 1;
         }
 
         // 2. + 1
         if (plusPosition < MAX && !visited[plusPosition]){
-            q.push(make_pair(plusPosition,time + 1));
+            q.push(plusPosition);
             memo[plusPosition] = curPosition;
+            visited[plusPosition] = visited[curPosition] + 1;
         }
 
         // 3. * 2
         if (multiPosition < MAX && !visited[multiPosition]) {
-            q.push(make_pair(multiPosition,time + 1));
+            q.push(multiPosition);
             memo[multiPosition] = curPosition;
+            visited[multiPosition] = visited[curPosition] + 1;
         }
     }
 
-    cout << result << endl;
-    for (int i = result + 1; i >= 0; i--) {
+    cout << visited[K] << endl; // 최단 시간 출력
+    for (int i = visited[K]; i >= 0; i--) { // 이동 과정 공백 포함해서 출력
         cout << ans_vector[i] << " ";
     }
 
@@ -76,7 +72,6 @@ void bfs() {
 
 int main() {
     cin >> N >> K; // 입력 받기
-    fill_n(visited, MAX, false); // 방문 배열 전부 false로 초기화
     bfs();
 
     return 0;
