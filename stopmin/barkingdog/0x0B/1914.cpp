b8e361b7b03e1a46bc4a0bcc1cc41e1232efa611
stopmin <geemin0@naver.com>
2024-08-17 16:33:18 +0900

    feat: 1914 설명 추가

--- stopmin/barkingdog/0x0B/1914.cpp
+++ stopmin/barkingdog/0x0B/1914.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 
-void func(int n, int a, int b) {
+void func(int n, int a, int b) { // a -> b (n개)
     if (n == 1) {
         printf("%d %d\n", a, b);
         return;
     }
 
     func(n - 1, a, 6 - a - b);
-    printf("%d %d\n", a, b);
+    printf("%d %d\n", a, b); // n번 원판을 기둥 a에서 b로 옮긴다.
     func(n - 1, 6 - a - b, b);
 }
 
