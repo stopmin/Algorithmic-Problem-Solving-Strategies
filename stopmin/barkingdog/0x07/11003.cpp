8d516e87848324bdce5c073aa665c185f576089a
stopmin <geemin0@naver.com>
2024-08-26 20:34:14 +0900

    feat: 11003 최솟값 찾기 주석

--- stopmin/barkingdog/0x07/11003.cpp
+++ stopmin/barkingdog/0x07/11003.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 struct cmp{
     bool operator()(const pair<int,int>&a, const pair<int,int>&b){
-        if(a.second != b.second) return a.second > b.second;
-        else return a.first > b.first;
+        if(a.second != b.second) return a.second > b.second; // 실제 값 기준
+        else return a.first > b.first; // 인덱스 기준
     }
 };
 int main(void) {
@@ -24,6 +24,7 @@ int main(void) {
         pq.push({i, val});
         while(pq.top().first <= i - l) pq.pop();
 
-        cout << pq.top().second << ' ';
+//        cout << pq.top().second << ' ';
+        cout<<pq.top().first<<" "<<pq.top().second<<endl;
     }
 }
