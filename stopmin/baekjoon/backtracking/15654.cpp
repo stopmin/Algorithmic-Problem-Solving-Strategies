f21fcc005beffc25cbf8f4cfc8c5fc98fce9bc2d
stopmin <geemin0@naver.com>
2025-01-26 22:12:55 +0900

    feat: 15654

---
+++ stopmin/baekjoon/backtracking/15654.cpp
@@ -0,0 +1,39 @@
+// https://www.acmicpc.net/problem/15654
+
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+vector<int> v;
+vector<int> num;
+vector<int> used;
+
+void dfs() {
+    if (v.size() == m) {
+        for (int i: v) cout << i << " ";
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!used[i]){
+            used[i] = true;
+            v.push_back(num[i]);
+            dfs();
+            v.pop_back(); // 백트래킹
+            used[i] = false;
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    num.resize(n);
+    used.resize(n, false);
+
+    for (int i = 0; i < n; i++) cin >> num[i];
+    sort(num.begin(), num.end());
+    dfs();
+}
